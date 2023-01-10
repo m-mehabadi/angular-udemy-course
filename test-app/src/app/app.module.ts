a377ad29b1ffdbed26690a0203019857b51601b5
mehabadi <ms.mehabadi@gmail.com>
2023-01-10 08:49:33 -0400

    Added a sidebar-component using CLI  command

--- test-app/src/app/app.module.ts
+++ test-app/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
